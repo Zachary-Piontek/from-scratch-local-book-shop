b042d3a0c109d04ee48a7fedb566287707a00942
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-02 15:28:42 -0700

    added books getAll to model, controller, test but still 404

---
+++ __tests__/books.test.js
@@ -0,0 +1,23 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('#GET /books returns list of books', async () => {
+    const resp = await request(app).get('/books');
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(Array),
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
