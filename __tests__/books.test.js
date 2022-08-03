bea43a808bd4944fd9b865c8d07efda20253a344
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-02 17:50:08 -0700

    added .post and insert for books and test passing

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -15,11 +15,29 @@ describe('backend-express-template routes', () => {
 
   it('#GET /books:id returns a book', async () => {
     const resp = await request(app).get('/books/1');
-    console.log(resp.body);
+    const bookOne = {
+      authors: [{
+        dob: 'Not Found',
+        id: 1,
+        name: 'Saifedean Ammous',
+        pob: 'Palestine'
+      }],
+      id: '1',
+      title: 'The Bitcoin Standard',
+      released: 2018,
+    };
+    expect(resp.body).toEqual(bookOne);
+  });
+
+  it('POST /books create new book', async () => {
+    const resp = await request(app)
+      .post('/books')
+      .send({ title: '10,000 Hours of Coding', released: 2022 });
     expect(resp.body).toEqual({
-      id: expect.any(String),
-      title: expect.any(String),
-      released: expect.any(Number),
+      authors: expect.any(Array),
+      id: '7',
+      title: '10,000 Hours of Coding',
+      released: 2022,
     });
   });
 
