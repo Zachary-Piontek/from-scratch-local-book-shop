99054ba83b5ab3a591b326b0093490d45cf1a780
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-03 10:13:47 -0700

    changed authors test but failing 2

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -9,35 +9,31 @@ describe('backend-express-template routes', () => {
   });
 
   it('#GET /authors returns list of authors', async () => {
-    const resp = await request(app).get('/books');
-    expect(resp.body.length).toEqual(6);
+    const resp = await request(app).get('/authors');
+    expect(resp.body.length).toEqual(4);
   });
 
   it('#GET /authors:id returns a authors', async () => {
     const resp = await request(app).get('/authors/1');
     const authorOne = {
-      authors: [{
-        dob: 'Not Found',
-        id: 1,
-        name: 'Saifedean Ammous',
-        pob: 'Palestine'
-      }],
       id: '1',
-      title: 'The Bitcoin Standard',
-      released: 2018,
+      name: 'Saifedean Ammous',
+      dob: 'Not Found',
+      pob: 'Palestine'
     };
     expect(resp.body).toEqual(authorOne);
   });
 
   it('POST /author create new author', async () => {
     const resp = await request(app)
-      .post('/books')
-      .send({ title: '10,000 Hours of Coding', released: 2022 });
+      .post('/authors')
+      .send({ name: 'Jordan Peterson', dob: '02-25-1960', pob: 'Canada' });
     expect(resp.body).toEqual({
-      authors: expect.any(Array),
-      id: '7',
-      title: '10,000 Hours of Coding',
-      released: 2022,
+      books: expect.any(Array),
+      id: '5',
+      name: 'Jordan Peterson',
+      dob: '02-25-1960',
+      pob: 'Canada'
     });
   });
 
